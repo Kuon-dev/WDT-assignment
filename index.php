72d83565f0e0525c3cb94e86291b9614005be40b
Remicaster <83005177+Remicaster@users.noreply.github.com>
2021-09-29 15:41:39 +0800

    some changes

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
         </header>
         <div class="content-area">
           <div>
-            <h2>contents</h2>
+            <h2>content area</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, esse est iste magni reprehenderit saepe sint! Assumenda consectetur fugit sit tempore. Architecto blanditiis deleniti, est fuga iusto perspiciatis suscipit voluptatibus! Ad alias assumenda autem deleniti esse est excepturi hic itaque, magnam magni nam officia provident quae recusandae reiciendis rem sed. Adipisci animi aut autem deserunt dolore, ea eius error ex facilis inventore ipsum minus modi nihil nostrum nulla, obcaecati porro reprehenderit repudiandae sed sint sunt tempore vero. At dolores doloribus, dolorum ea eaque hic ipsa iusto libero minus natus neque nisi nobis nostrum odio optio pariatur perspiciatis praesentium rerum sapiente.</p>
             <br>
             <br>
@@ -70,28 +70,28 @@
                     <td>
                         <div class="imgContainer">
                             <a href="https://playvalorant.com/en-us/">
-                                <img  class="IMG" src="whiskas-adult-ocean-fish.jpg">
+                                <img  class="IMG" src="image/whiskas-adult-ocean-fish.jpg">
                             </a>
                         </div>
                     </td>
                     <td>
                         <div class="imgContainer">
                             <a href="https://playvalorant.com/en-us/">
-                                <img  class="IMG" src="whiskas-adult-ocean-fish.jpg">
+                                <img  class="IMG" src="image/whiskas-adult-ocean-fish.jpg">
                             </a>
                         </div>
                     </td>
                     <td>
                         <div class="imgContainer">
                             <a href="https://playvalorant.com/en-us/">
-                                <img  class="IMG" src="whiskas-adult-ocean-fish.jpg">
+                                <img  class="IMG" src="image/whiskas-adult-ocean-fish.jpg">
                             </a>
                         </div>
                     </td>
                     <td>
                         <div class="imgContainer">
                             <a href="https://playvalorant.com/en-us/">
-                                <img  class="IMG" src="whiskas-adult-ocean-fish.jpg">
+                                <img  class="IMG" src="image/whiskas-adult-ocean-fish.jpg">
                             </a>
                         </div>
                     </td>
@@ -141,33 +141,37 @@
                         <button class="addToCart">Add to Cart</button>
                     </td>
                 </tr>
+            </table>
+            <br>
+            <hr>
+            <table class="productContainer">
                 <!-- 5th Row -->
                 <tr>
                     <td>
                         <div class="imgContainer">
                             <a href="https://playvalorant.com/en-us/">
-                                <img class="IMG" src="whiskas-adult-ocean-fish.jpg">
+                                <img class="IMG" src="image/whiskas-adult-ocean-fish.jpg">
                             </a>
                         </div>
                     </td>
                     <td>
                         <div class="imgContainer">
                             <a href="https://playvalorant.com/en-us/">
-                                <img class="IMG" src="whiskas-adult-ocean-fish.jpg">
+                                <img class="IMG" src="image/whiskas-adult-ocean-fish.jpg">
                             </a>
                         </div>
                     </td>
                     <td>
                         <div class="imgContainer">
                             <a href="https://playvalorant.com/en-us/">
-                                <img class="IMG" src="whiskas-adult-ocean-fish.jpg">
+                                <img class="IMG" src="image/whiskas-adult-ocean-fish.jpg">
                             </a>
                         </div>
                     </td>
                     <td>
                         <div class="imgContainer">
                             <a href="https://playvalorant.com/en-us/">
-                                <img class="IMG" src="whiskas-adult-ocean-fish.jpg">
+                                <img class="IMG" src="image/whiskas-adult-ocean-fish.jpg">
                             </a>
                         </div>
                     </td>
