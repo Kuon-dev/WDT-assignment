70aea6d43f072228d6cde43c0a7457b07d53cf91
Remicaster <83005177+Remicaster@users.noreply.github.com>
2021-09-30 21:03:48 +0800

    edited FAQ

--- faq.php
+++ faq.php
@@ -15,50 +15,83 @@
             });
 
         </script>
+        <style type="text/css">
+        .content-area ul {
+            list-style: none;
+        }
+        .content-area ul li:before {
+            background-position: 0 5px;
+            line-height: 1.5625;
+            padding: 0 0 4px 30px;
+            margin-left: 1em;
+        }
+        </style>
     </head>
     <body>
     <div id="header"></div>
-    <div class="content-area">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     <img src="image/faqs.jpeg" width="1000px">
-            <br>
-            <h2 class="font-sora fs-2 fw-bold" align="left">General FAQs</h2>
-            <br>
-            <h3 class="font-sora fs-2 fw-bold" align="left">Can I change or cancel my order?</h3>
-            <p class="font-sora" align="left">Once the order has been placed, we are unable to amend the items in your order.
+    <br>
+    <br>
+    <div class="a-divider"></div>
+    <div class="content-area" style="margin-top: -300px; padding: 250px">
+        <ul>
+            <li><h3 class="font-sora fs-3 fw-bolder" align="justify"- >- Can I change or cancel my order?</h3></li>
+        </ul>
+            <p class="font-sora fs-4 text-opacity-50" align="justify">Once the order has been placed, we are unable to amend the items in your order.
             Once the order has been picked and packed by our warehouse teams, we are unable to cancel the order. 
             Should you need to cancel your order, please contact our Customer Service Team to see if this is a possibility.</p>
             <br>
-            <h3 class="font-sora fs-2 fw-bold" align="left">Can I change the delivery address on the order?</h3>
-            <p class="font-sora" align="left">If your order has just been placed, please contact our Customer Service Team to amend the delivery address.
+        <ul>
+            <li><h3 class="font-sora fs-3 fw-bolder" align="justify">- Can I change the delivery address on the order?</h3></li>
+        </ul>
+            <p class="font-sora fs-4 " align="justify">If your order has just been placed, please contact our Customer Service Team to amend the delivery address.
             If your order has already been processed and dispatched it cannot be amended by Paws Heaven. Once the order has been dispatched, we recommend contacting the Courier Partner to attempt to redirect the parcel. 
             This may delay the expected delivery time.</p>
             <br>
-            <h3 class="font-sora fs-2 fw-bold" align="left">How can I modify my personal details?</h3>
-            <p class="font-sora" align="left">To modify personal details, you may go to "My Account">"Modify Personal Details".</p>
+        <ul>
+            <li><h3 class="font-sora fs-3 fw-bolder" align="justify">- How can I modify my personal details?</h3></li>
+        </ul>
+            <p class="font-sora fs-4 " align="justify">To modify personal details, you may go to "My Account">"Modify Personal Details".</p>
             <br>
-            <h3 class="font-sora fs-2 fw-bold" align="left">What is the product warranty?</h3>
-            <p class="font-sora" align="left">We warranty our materials and workmanship. Our commitment is to your satisfaction with our products. In warranty or not, 
+        <ul>
+            <li><h3 class="font-sora fs-3 fw-bolder" align="justify">- What is the product warranty?</h3>
+        </ul>
+            <p class="font-sora fs-4 " align="justify">We warranty our materials and workmanship. Our commitment is to your satisfaction with our products. In warranty or not, 
             it is the culture of our company to address and resolve all customer issues to everyone’s satisfaction</p>
             <br>
-            <h3 class="font-sora fs-2 fw-bold" align="left">What kinds of payment methods do we accept?</h3>
-            <p class="font-sora" align="left">We accept online banking and payment through debit/credit card.</p>
+        <ul>
+            <li><h3 class="font-sora fs-3 fw-bolder" align="justify">- What kinds of payment methods do we accept?</h3></li>
+        </ul>
+            <p class="font-sora fs-4 " align="justify">We accept online banking and payment through debit/credit card.</p>
             <br>
-            <h3 class="font-sora fs-2 fw-bold" align="left">Shipping & Delivery Timeframes</h3>
-            <p class="font-sora" align="left">All standard shipping is sent via ABX Express. We aim to have orders packed within 1-2 business days. 
+        <ul>
+            <li><h3 class="font-sora fs-3 fw-bolder" align="justify">- Shipping & Delivery Timeframes</h3></li>
+        </ul>
+            <p class="font-sora fs-4 " align="justify">All standard shipping is sent via ABX Express. We aim to have orders packed within 1-2 business days. 
             Your order is expected to be delivered in 2-5 business days within West Malaysia. Regional destinations may take up to 7 business days.</p>
             <br>
-            <h3 class="font-sora fs-2 fw-bold" align="left">Return Policy</h3>
+        <ul>
+            <li><h3 class="font-sora fs-3 fw-bolder" align="justify">- Return Policy</h3></li>
+        </ul>
             <dl>
-                <dt class="font-sora" align="left">Proof of purchase is required for all returns, which can be in the form of your receipt/tax invoice, a debit/credit card statement, member's club transaction history, or online purchase transaction history.
-                The products muast be returned within 28 days of purchase with original condition. You are entitled to a refund, replacement or credit from Paws Heaven when an item purchased have any issues, i.e. the product:
-                    <dd class="font-sora" align="left">- Is faulty or of unacceptable quality</dd>
-                    <dd class="font-sora" align="left">- Does not match the description or sample</dd>
-                    <dd class="font-sora" align="left">- Is unfit for the intended purpose (i.e. does not do what it is meant to do)</dd>
-                    <dd class="font-sora" align="left">- Is not delivered in a reasonable time frame</dd>
-                    <dd class="font-sora" align="left">- Proof of purchase must be provided (proof of purchase details are outlined above)</dd>
+                <p class="font-sora fs-4 " align="justify">Proof of purchase is required for all returns, which can be in the form of your receipt/tax invoice, a debit/credit card statement, member's club transaction history, or online purchase transaction history.
+                The products muast be returned within 28 days of purchase with original condition. You are entitled to a refund, replacement or credit from Paws Heaven when an item purchased have any issues, i.e. the product:</p>
+                <dt>
+                    <dd class="font-sora fs-5" align="justify">- Is faulty or of unacceptable quality</dd>
+                    <dd class="font-sora fs-5" align="justify">- Does not match the description or sample</dd>
+                    <dd class="font-sora fs-5" align="justify">- Is unfit for the intended purpose (i.e. does not do what it is meant to do)</dd>
+                    <dd class="font-sora fs-5" align="justify">- Is not delivered in a reasonable time frame</dd>
+                    <dd class="font-sora fs-5" align="justify">- Proof of purchase must be provided (proof of purchase details are outlined above)</dd>
                 </dt>
             </dl>
-        </div>
+        </ul>
+    </div>
     </body>
-    <div id="footer"></div>
+    <div id="footer" style="margin-top: -250px;"></div>
 </html>
