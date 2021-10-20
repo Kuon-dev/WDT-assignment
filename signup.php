e89d89e6ebc2df632c082da997232f7c5085cdbb
katherine0241 <83446472+katherine0241@users.noreply.github.com>
2021-10-20 23:32:28 +0800

    Updated

--- signup.php
+++ signup.php
@@ -1,16 +1,33 @@
 <?php
     include("conn.php");
-    $sql="INSERT INTO member (Email, Member_Name, Contact_Number, Address,Password)
-    VALUES
-    ('$_POST[SignUp_email]','$_POST[SignUp_name]','$_POST[SignUp_phoneNum]','$_POST[SignUp_address]','$_POST[SignUp_password]')";
-    if (!mysqli_query($con,$sql)) {
-    die('Error: ' . mysqli_error($con));
+    $SignUp_name=$_POST["SignUp_name"];
+    $SignUp_phoneNum=$_POST["SignUp_phoneNum"];
+    $SignUp_email=$_POST["SignUp_email"];
+    $SignUp_address=$_POST["SignUp_address"];
+    $SignUp_password=$_POST["SignUp_password"];
+
+    $result=mysqli_query($con,"SELECT * FROM member WHERE Email='$SignUp_email' limit 1");
+
+    if(mysqli_num_rows($result)==1){
+        echo "<script>
+        alert('Account exists.');
+        window.location.href = 'registration_panel.php';
+        </script>";
+        mysqli_close($con);
     }
-    else {
-    session_start();
-    echo '<script>
-    window.location.href = "common/Loading_screen.html";
-    </script>';
-    }
-    mysqli_close($con);
+    else{
+        $sql="INSERT INTO member (Email, Member_Name, Contact_Number, Address,Password)
+        VALUES
+        ('$_POST[SignUp_email]','$_POST[SignUp_name]','$_POST[SignUp_phoneNum]','$_POST[SignUp_address]','$_POST[SignUp_password]')";
+        if (!mysqli_query($con,$sql)) {
+            die('Error: ' . mysqli_error($con));
+            }
+            else {
+            session_start();
+            echo '<script>
+            window.location.href = "dumbass_page.php";
+            </script>';
+            }
+        }
+        mysqli_close($con);
 ?>
