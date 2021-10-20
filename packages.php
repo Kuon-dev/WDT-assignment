cad070f38f9171e783de50a40d93937eac608cdf
katherine0241 <83446472+katherine0241@users.noreply.github.com>
2021-10-20 16:09:53 +0800

    Create packages.php

---
+++ packages.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Page Title -->
+    <title>Paws Heaven</title>
+
+    <!-- Common Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Import CSS stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Import jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
+    <!-- Header & Footer Loader -->
+    <script>
+        $(function(){ 
+            $("#header").load("common/header.php");
+            $("#footer").load("common/footer.php");
+        });
+    
+        function redicrect() {
+            window.location.href = "https://playvalorant.com/en-us/";
+        }
+    </script>
+
+    <!-- Styling -->
+    <style>
+    .banner-area {
+        width: 100%;
+        height: 450px;
+        background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4)), url(image/about-us.jpg);
+        -webkit-background-size: cover;
+        background-size: cover;
+        background-position: center center;
+        padding-bottom: 10px;
+        margin-top: 110px;
+        background-color: black;
+    }
+
+    .banner-area h2 {
+        padding-top: 8%;
+        font-size: 70px;
+        filter: brightness(2000%);
+        color: white;
+    }
+
+    .parentbox {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .childbox {
+        /*
+        flex: flex-grow flex-shrink flex-basis;
+        flex-grow 	A number specifying how much the item will grow relative to the rest of the flexible items
+        flex-shrink 	A number specifying how much the item will shrink relative to the rest of the flexible items
+        flex-basis 	The length of the item. Legal values: "auto", "inherit", or a number followed by "%", "px", "em" or any other length unit
+        */
+        flex: 0 0 300px;
+        margin: 25px;
+        height: 450px;
+        background-color: white;
+        border-radius: 10px;
+        border: #d4d4d4 solid 2px;
+        padding: 10px;
+    }
+
+    .childbox hr {
+        margin: 5px;
+    }
+
+    .childbox:hover {
+        box-shadow: 0 8px 17px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+    }
+
+    .productName {
+        height: 55px;
+    }
+
+    .productName:hover {
+        font: black; 
+    }
+
+    .productPrice {
+        height: 30px;
+        margin-top: 20px;
+    }
+
+    .addtocartBTN {
+        border: 1px solid black;
+        background-color: white;
+        width: 150px;
+        height: 40px;
+        text-align: center;
+        
+    }
+
+    .addtocartBTN:hover {
+        cursor: pointer;
+        background-color: grey;
+        transition: background-color 0.5s ease-in; 
+    }
+    </style>
+
+</head>
+<body>
+<div id="header"></div>
+<div class="a-divider"></div>
+<div class="content-area" style="margin-top: -300px; padding: 250px; padding-bottom: 10px">
+        <div>
+        <h2 class="sora center fs-1 fw-bolder">Packages</h2>
+        <br>
+        <hr>
+        </div>
+        <div class="parentbox">
+        <?php 
+            include ("conn.php");
+            $result=mysqli_query($con,"SELECT * FROM product ORDER BY Product_Name");
+            while($row=mysqli_fetch_array($result)){
+                
+                if ($row['Type of Animal']=="Cat"){
+                    $product_img = "whiskas-adult-ocean-fish.jpg";
+                }
+                else if ($row['Type of Animal']=="Dog") {
+                    $product_img = "dog.png";
+                }
+                
+                if ($row['Category']=="Packages"){
+                $data = '<div class="childbox" onclick="redirect()">
+                <img src="image/'.$product_img.'" width= "200px" height="240px style="float: center">
+                
+                <hr>
+
+                <div class="productName">
+                    <a href="#">
+                    <h5>'.$row['Product_Name'].'</h5></a>
+                </div>
+
+                <div class="productPrice">
+                    RM '.$row['Price'].'<br>
+                </div>
+
+                <a href="#">
+                    <button class="addtocartBTN">Add To Cart</button>
+                </a>
+                </div>
+                
+                ';
+                
+                echo $data;
+                
+                } 
+            }
+                mysqli_close($con);//to close the database connection
+        ?>
+        </div>
+
+</div>
+
+</body>
+
+    <!-- Footer -->
+    <div id="footer"></div>
+</html>
