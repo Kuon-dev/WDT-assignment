444834d35ef2072a9a5fcc9a6b04773b346c36f7
katherine0241 <83446472+katherine0241@users.noreply.github.com>
2021-11-02 22:13:44 +0800

    Update purchase-history.php

--- purchase-history.php
+++ purchase-history.php
@@ -1,4 +1,5 @@
 <?php
+include("conn.php"); 
 ob_start();
 session_start();
 ?>
@@ -36,18 +37,39 @@ session_start();
             <div class="col-md text-light fs-5 fw-bold sora" style="text-align: right">Order ID</div>
         </div>
         <!-- Order items Items -->
-
-        <div class="row bg-secondary align-items-center" style="align-items: center; padding: 7px 0px">
-            <div class="col-md-5 text-light" style="text-align: left">Product Name</div>
-            <div class="col-md text-light" style="text-align: right">Quantity</div>
-            <div class="col-md text-light" style="text-align: right">Transaction Date</div>
-            <div class="col-md text-light" style="text-align: right">Order ID</div>
-            <div class="w-100"></div>
-            <div class="col-md-5 text-light" style="text-align: left">Product Name</div>
-            <div class="col-md text-light" style="text-align: right">Quantity</div>
-            <div class="col-md text-light" style="text-align: right">Transaction Date</div>
-            <div class="col-md text-light" style="text-align: right">Order ID</div>
-        </div>
+        <?php 
+            //connect database
+            include ("conn.php");
+            //get data from database
+            if(isset($_SESSION["user_email"])){
+                $email = $_SESSION["user_email"];
+                $sql_run=mysqli_query($con, "SELECT * FROM orders WHERE Email = '$email'");
+                while ($row=mysqli_fetch_assoc($sql_run)) {
+                    $Product_ID=$row['Product_ID'];
+                    $Quantity=$row['Quantity'];
+                    $Purchase_Date=$row['Purchase_Date'];
+                    $Order_ID=$row['Order_ID'];
+                    $mysql_run=mysqli_query($con, "SELECT * FROM product WHERE Product_ID='$Product_ID'");
+                    while ($row=mysqli_fetch_assoc($mysql_run)) {
+                        $Product_Name=$row["Product_Name"];
+                echo '
+                <div class="row bg-secondary align-items-center" style="align-items: center; padding: 7px 0px">
+                    <div class="col-md-5 text-light" style="text-align: left">'.$Product_Name.'</div>
+                    <div class="col-md text-light" style="text-align: right">'.$Quantity.'</div>
+                    <div class="col-md text-light" style="text-align: right">'.$Purchase_Date.'</div>
+                    <div class="col-md text-light" style="text-align: right">'.$Order_ID.'</div>
+                    <div class="w-100"></div>
+                </div>';
+                    }
+                }
+            }
+            else{
+                echo"<script>
+                alert('You need to sign in first to access this page.');
+                window.location.href='registration_panel.php';
+                </script>";
+            }
+        ?>
     <!-- If no order Items -->
 
         </div> <!-- close entire table-->
